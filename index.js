cd674985889033cb06780cba09fa17c3e74b9e68
Quan <quantrung286@gmail.com>
2021-12-17 19:51:23 +0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const flexContainer = document.querySelector('.flex-container')
-const image = document.querySelector('.image')
-const description = document.querySelector('.description')
+
 const input = document.querySelector('input')
 const select = document.querySelector('select')
 const button = document.querySelector('button')
-
+const valueOption = document.querySelectorAll('option')
 
 button.addEventListener('click', e => {
-
-    let value = input.value
+    let value = Number(input.value)
     let plane = select.value
     if (plane === 'none' && !value) {
         flexContainer.innerHTML = 'You haven\'t choose a plane and input mass yet'
@@ -17,10 +15,27 @@ button.addEventListener('click', e => {
         flexContainer.innerHTML = 'You haven\'t choose a plane yet'
     } else if (!value) {
         flexContainer.innerHTML = 'Mass is required'
+    } else {
+        flexContainer.innerHTML = `
+        <div class="flex-item image"></div>
+        <div class="flex-item description"></div>
+        `
+        const image = document.querySelector('.image')
+        const description = document.querySelector('.description')
+        valueOption.forEach( eachPlane => {
+            let gravity = Number(eachPlane.dataset.gravity)
+            if (plane === eachPlane.value) {
+                image.innerHTML = `
+                <img src='./img/${plane}.png' class="planet-image" />
+                `
+                description.innerHTML = `
+                    The weight of the object on ${eachPlane.textContent}\n
+                    ${Math.round(value * gravity)}
+                `
+            }
+        })
     }
-    
-    // switch plane:
-    //     case 'earth'
+
 })
 
 
