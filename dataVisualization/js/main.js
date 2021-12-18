3d9b760dc9ae6f72de5f095e0627740966bab268
Quan <quantrung286@gmail.com>
2021-12-18 10:23:21 +0700

    update languages handle

--- dataVisualization/js/main.js
+++ dataVisualization/js/main.js
@@ -1,16 +1,21 @@
 const subtitle = document.querySelector('.subtitle')
-const totalPopulation = countries.reduce((acc, cur) => acc + cur.population, 0)
-const sortedByPopulation = countries.sort((a, b) => b.population - a.population)
 const population = document.querySelector('.population')
 const languages = document.querySelector('.languages')
 const graphTitle = document.querySelector('.graph-title')
 const graphs = document.querySelector('.graphs')
 const graphWrapper = document.querySelector('.graph-wrapper')
 
+subtitle.innerHTML = `Currently, we have ${countries.length} countries`
+
+// Population handle 
+const totalPopulation = countries.reduce((acc, cur) => acc + cur.population, 0)
+const sortedByPopulation = countries.sort((a, b) => b.population - a.population)
 
 population.addEventListener('click', () => {
     let numberOfCountries = Number(prompt('Input number of countries:', 10))
     let numberOfSortedCountries = sortedByPopulation.slice(0, numberOfCountries)
+
+
     graphTitle.textContent = `${numberOfCountries} Most populated countries in the world`
     graphs.innerHTML = `
         <div class="graph-wrapper">
@@ -23,7 +28,6 @@ population.addEventListener('click', () => {
         graphs.innerHTML += `
         <div class="graph-wrapper">
             <div class="graph-name">
-                <img src="${coun.flag}" alt="">
                 ${coun.name}
             </div>
             
@@ -34,26 +38,55 @@ population.addEventListener('click', () => {
     })
 })
 
-subtitle.innerHTML = `Currently, we have ${countries.length} countries`
-languages.addEventListener('click', () => {
-    // Filter out an array of all languages
-    let allLang = []
-    let mostSpokenLang = []
-    countries.forEach(coun => {
-        coun.languages.forEach( eachLang => {
-            allLang.push(eachLang)
-        })
+// Languages handle
+// Filter out an array of all languages
+let allLang = []
+let mostSpokenLang = []
+countries.forEach(coun => {
+    coun.languages.forEach( eachLang => {
+        allLang.push(eachLang)
     })
-    let uniqueLang = [ ... new Set(allLang)]
-    console.log(uniqueLang)
+})
+let uniqueLang = [ ... new Set(allLang)]
+// console.log(uniqueLang)
 
-    countries.forEach(coun => {
-        coun.languages.forEach( eachLang => {
-            
+uniqueLang.forEach(lang => {
+    mostSpokenLang.push({
+        name: lang,
+        count: 0
+    })
+})
+countries.forEach(coun => {
+    coun.languages.forEach( lang => {
+        mostSpokenLang.forEach( eachLang => {
+            if ( lang === eachLang.name ) {
+                eachLang.count++
+            }
         })
     })
 })
-// Phần coun.flag bị lỗi bên api
+mostSpokenLang.sort((a, b) => b.count - a.count)
+// console.log(mostSpokenLang)
+
+languages.addEventListener('click', () => {
+    graphs.innerHTML = ``
+    let numberOfLanguages = Number(prompt('Input number of languages:', 10))
+    let numberOfSortedLanguages = mostSpokenLang.slice(0, numberOfLanguages)
+
+    graphTitle.textContent = `${numberOfLanguages} Most spoken languages in the world`
+    numberOfSortedLanguages.forEach( (coun, index) => {
+        graphs.innerHTML += `
+        <div class="graph-wrapper">
+            <div class="graph-name">
+                ${coun.name}
+            </div>
+            
+            <div class="percentage percenOf${coun.name}" style="width: ${coun.count / numberOfSortedLanguages[0].count * 100 * 0.5}%;"></div>
+            <div class="graph-population">${coun.count}</div>
+        </div>
+        `
+    })
+})
 
 
 
