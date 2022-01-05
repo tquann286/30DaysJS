5a528d2751c129f3f910bbf53fca157cc94979b1
Quan <quantrung286@gmail.com>
2022-01-05 10:51:27 +0700

    update transition

--- box-modal/scripts/main.js
+++ box-modal/scripts/main.js
@@ -5,15 +5,29 @@ const btnClose = document.querySelector('.modal__footer button')
 
 console.log()
 
-const toggleModal = (e) => {
-    modal.classList.toggle('hide')
+// const toggleModal = (e) => {
+//     modal.classList.toggle('hide')
+// }
+
+const showModal = () => {
+    modal.classList.add('show')
 }
 
-btnOpen.addEventListener('click', toggleModal)
-iconClose.addEventListener('click', toggleModal)
-btnClose.addEventListener('click', toggleModal)
-modal.addEventListener('click', (e) => {
-    if (e.target === e.currentTarget) {
-        toggleModal()
+const hideModal = () => {
+    modal.classList.remove('show')
+}
+
+btnOpen.addEventListener('click', showModal)
+iconClose.addEventListener('click', hideModal)
+btnClose.addEventListener('click', hideModal)
+// modal.addEventListener('click', (e) => {
+//     if (e.target === e.currentTarget) {
+//         toggleModal()
+//     }
+// })
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+      modal.classList.remove('show');
     }
-})
+}
