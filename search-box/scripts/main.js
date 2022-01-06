4d92c21f015db229b8666e3213fe746bb68c6027
Quan <quantrung286@gmail.com>
2022-01-06 21:34:42 +0700

    clicked outside detect

--- search-box/scripts/main.js
+++ search-box/scripts/main.js
@@ -4,4 +4,12 @@ btnSearch.addEventListener('click', function() {
     // Arrow function không có 'this'
     this.parentElement.classList.toggle('open')
     this.previousElementSibling.focus()
+    console.log('cc')
 })
+
+window.onclick = function(event) {
+    if (!btnSearch.parentElement.contains(event.target)) {
+        console.log('cl')
+        btnSearch.parentElement.classList.remove('open')
+    }
+}
