7c5ac41384825588cb030244bb0399e2a485f88b
Quan <quantrung286@gmail.com>
2022-01-07 23:02:25 +0700

    up

---
+++ register-form/scripts/main.js
@@ -0,0 +1,5 @@
+const username = document.querySelector('#username')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+const confirmPassword = document.querySelector('#confirm-password')
+
