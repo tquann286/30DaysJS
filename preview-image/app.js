83ba67c31f79298784ae59f717f230288bb11f00
Quan <quantrung286@gmail.com>
2022-02-23 22:45:57 +0700

    up

--- preview-image/app.js
+++ preview-image/app.js
@@ -4,5 +4,6 @@ const preview = document.querySelector('.preview')
 upload.addEventListener('change', (e) => {
   const img = document.createElement('img')
   img.src = URL.createObjectURL(upload.files[0])
+
   preview.appendChild(img)
 })
